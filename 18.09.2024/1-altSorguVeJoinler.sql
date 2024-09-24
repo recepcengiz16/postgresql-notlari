8a8cfb5bf2a7847bd9b4dae815f930600cf6890d
Recep <recepcengiz_96@outlook.com>
2024-09-24 13:55:35 +0300

    yorum satırında düzenleme yapıldı

--- 18.09.2024/1-altSorguVeJoinler.sql
+++ 18.09.2024/1-altSorguVeJoinler.sql
@@ -6,9 +6,12 @@
 
 --PostgreSQL'de sütun ve tablo adlarında büyük/küçük harf duyarlılığı ile ilgili bir kural vardır: 
 --eğer bir sütun adı çift tırnak içinde yazılırsa("fakulteId" gibi), büyük/küçük harf duyarlılığı korunur. 
---Aksi takdirde, PostgreSQL sütun ve tablo adlarını otomatik olarak küçük harfe çevirir(fakulteid gibi) ve sütun bulunamazsa hata verir.
+--Aksi takdirde, PostgreSQL sütun ve tablo adlarını otomatik olarak küçük harfe çevirir(fakulteid gibi) 
+--ve sütun bulunamazsa hata verir.
 
---Kodda "fakulteId" sütunu çift tırnak içinde yazıldığı için, bu sütun adı büyük/küçük harfe duyarlıdır ve veritabanında tam olarak "fakulteId" olarak tanımlanmıştır. Eğer çift tırnak kullanılmasaydı, PostgreSQL bu sütun adını otomatik olarak küçük harfe dönüştürerek fakulteid olarak yorumlardı.
+--Kodda "fakulteId" sütunu çift tırnak içinde yazıldığı için, bu sütun adı büyük/küçük harfe duyarlıdır 
+--ve veritabanında tam olarak "fakulteId" olarak tanımlanmıştır.
+--Eğer çift tırnak kullanılmasaydı, PostgreSQL bu sütun adını otomatik olarak küçük harfe dönüştürerek fakulteid olarak yorumlardı.
 
 select count(*),fakulte.ad from bolum 
 inner join fakulte on bolum."fakulteId" = fakulte.id 
