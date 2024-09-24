8ad6a0f9d577b8fab2ccc41e8809245730f863de
Recep <recepcengiz_96@outlook.com>
2024-09-24 09:22:47 +0300

    yorum satırı eklendi

--- 18.09.2024/1-altSorguVeJoinler.sql
+++ 18.09.2024/1-altSorguVeJoinler.sql
@@ -4,6 +4,12 @@
 
 --select count(*),fakulte.ad from bolum inner join fakulte on bolum."fakulteId" = fakulte.id group by fakulte.ad
 
+--PostgreSQL'de sütun ve tablo adlarında büyük/küçük harf duyarlılığı ile ilgili bir kural vardır: 
+--eğer bir sütun adı çift tırnak içinde yazılırsa("fakulteId" gibi), büyük/küçük harf duyarlılığı korunur. 
+--Aksi takdirde, PostgreSQL sütun ve tablo adlarını otomatik olarak küçük harfe çevirir ve sütun bulunamazsa hata verir.
+
+Kodda "fakulteId" sütunu çift tırnak içinde yazıldığı için, bu sütun adı büyük/küçük harfe duyarlıdır ve veritabanında tam olarak "fakulteId" olarak tanımlanmıştır. Eğer çift tırnak kullanılmasaydı, PostgreSQL bu sütun adını otomatik olarak küçük harfe dönüştürerek fakulteid olarak yorumlardı.
+
 select count(*),fakulte.ad from bolum 
 inner join fakulte on bolum."fakulteId" = fakulte.id 
 group by fakulte.ad order by count(*) desc limit 1 -- en fazla bölümü olan fakulte nedir
