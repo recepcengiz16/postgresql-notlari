c25d9b3231a3f5419d3b27325b25470b77fffe57
Recep Cengiz <recepcengiz_96@outlook.com>
2024-11-05 15:11:52 +0300

    yorum satırı eklendi

--- 17.09.2024/11-truncateKullanimi.sql
+++ 17.09.2024/11-truncateKullanimi.sql
@@ -2,3 +2,4 @@
 --Eğer tabloda bir AUTO_INCREMENT sütunu varsa (örneğin, otomatik artan bir ID sütunu), TRUNCATE komutu bu değeri sıfırlar.
 
 truncate table urun
+restart identity; -- bu şekilde de ilerleyen autoincrement değerini sıfıra çekiyoruz.
